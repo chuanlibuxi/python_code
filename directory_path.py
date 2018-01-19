9bd725eb0ec57c01bbaf112609ad7154def03638
chuanlibuxi <18202926953@163.com>
2018-01-19 18:10:00 +0800

    practice

---
+++ directory_path.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+
+def print_directory_contents(sPath):
+    """
+        这个函数接受文件夹的名称作为输入参数，
+        返回该文件夹中文件的路径，
+        以及其包含文件夹中文件的路径。
+        """
+    # 补充代码
+    import os
+    for sChild in os.listdir(sPath):
+        sChildPath = os.path.join(sPath, sChild)
+        if os.path.isdir(sChildPath):
+            print_directory_contents(sChildPath)
+        else:
+            print(sChildPath)
+
+print_directory_contents(r'E:\PythonCode\DataStuctures')
